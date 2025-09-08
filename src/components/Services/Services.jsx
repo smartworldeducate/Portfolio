062c7eff3d2f8c83f405ac2e6af300472824d817
salmanali <sa5882218@gmail.com>
2025-09-08 13:49:47 +0500

    chnage card  text

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -72,7 +72,7 @@ const Services = () => {
             emoji={Humble}
             heading={"UI/UX"}
             detail={
-              "Lorem ispum dummy text are usually use in section where we need some random text"
+              "UNIQE DESIGNS, WEB APPS, MOBILE APPS"
             }
             color="rgba(252, 166, 31, 0.45)"
           />
